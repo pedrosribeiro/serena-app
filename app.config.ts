78a3ef6ee1c8e8f1fe9a11b2b869ae6c4ccdbc93
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2025-06-26 11:22:46 -0300

    build: fix owner

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,7 @@ export default {
     name: 'serena-app',
     slug: 'serena-app',
     version: '1.0.0',
+    owner: 'pribeiroo',
     android: {
       package: 'com.pribeiroo.serenaapp',
     },
@@ -12,7 +13,7 @@ export default {
       EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
       API_URL: process.env.EXPO_PUBLIC_API_URL,
       eas: {
-        projectId: '5cea8237-f55b-43c1-89bb-4a812737063c',
+        projectId: '5cea8237-f55b-43c1-89bb-4a812737063c'
       },
     },
   },
