934c7468d8a885cba8990b5cbf675971db430c17
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2025-06-26 10:19:37 -0300

    build: eas config

--- app.config.ts
+++ app.config.ts
@@ -5,8 +5,14 @@ export default {
     name: 'serena-app',
     slug: 'serena-app',
     version: '1.0.0',
+    android: {
+      package: 'com.pribeiroo.serenaapp',
+    },
     extra: {
-      API_URL: process.env.API_URL,
+      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL,
+      eas: {
+        projectId: '5cea8237-f55b-43c1-89bb-4a812737063c',
+      },
     },
   },
 };
